b1177689d5f32ccb88a06e0f7aa91926f757e3a3
Arthur <turods@yandex.ru>
2024-07-24 21:48:29 +0300

    Rearrange to pipeline

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,10 @@
 from typing import Generator
-from itertools import chain
 from datasets import load_dataset, concatenate_datasets, Dataset
-from tokenizers import ByteLevelBPETokenizer, Tokenizer, normalizers, Regex, models, pre_tokenizers, trainers, processors, decoders
+from tokenizers import ByteLevelBPETokenizer, Tokenizer, normalizers, Regex
 from operator import attrgetter
 from tqdm import tqdm
 import torch
 import os
-
-
-# TODO: arrange this file as a pipeline
 
 
 def data_iterator(batch_size: int, dataset: Dataset) -> Generator[list[str], None, None]:
@@ -55,12 +51,7 @@ def tokenize_dataset(dataset: Dataset,
     return shard_size * k + p
 
 
-
-def pipeline():
-    pass
-
-
-def main() -> None:
+def pipeline(vocab_size: int = 8192, data_root: str = 'data', tokenizer_root: str = 'tokenizer') -> Tokenizer:
 
     # Collect the data
 
@@ -68,14 +59,12 @@ def main() -> None:
 
     # Tokenizer
     
-    # tokenizer = ByteLevelBPETokenizer()
-    # tokenizer.normalizer = normalizers.Replace(Regex('[[:space:]]+'), ' ')
-    # tokenizer.train_from_iterator(data_iterator(30, habr['train']), vocab_size=8192, min_frequency=2, special_tokens=['[EOS]'], show_progress=True)
-    # tokenizer.save('tokenizer.json')
-    
-    tokenizer = Tokenizer.from_file('tokenizer.json')
+    tokenizer = ByteLevelBPETokenizer()
+    tokenizer.normalizer = normalizers.Replace(Regex('[[:space:]]+'), ' ')
+    tokenizer.train_from_iterator(data_iterator(30, habr['train']), vocab_size=vocab_size, min_frequency=2, special_tokens=['[EOS]'], show_progress=True)
+    tokenizer.save(f'{tokenizer_root}/tokenizer.json')
 
-    # Datasets splits and merges
+    # Dataset split
 
     habr_split = habr['train'].train_test_split(0.05)
 
@@ -84,19 +73,24 @@ def main() -> None:
 
     # Tokenization
 
+    os.makedirs(f'{data_root}/train', exist_ok=True)
+    os.makedirs(f'{data_root}/val', exist_ok=True)
+    
     batch_size = 50
     shard_size = 2*10**7 
-    path = 'data/val'
+    path = f'{data_root}/val'
     eos_tok = '[EOS]'
 
     total_val_tokens = tokenize_dataset(data_val, tokenizer, path, batch_size, shard_size, eos_tok)
     print(f'Total validation tokens: {total_val_tokens}')
 
-    path = 'data/train'
+    path = f'{data_root}/train'
     shard_size = 10**8
 
     total_train_tokens = tokenize_dataset(data_train, tokenizer, path, batch_size, shard_size, eos_tok)
     print(f'Total train tokens: {total_train_tokens}')
 
+    return tokenizer
+
 if __name__ == '__main__':
-    main()
+    pipeline()
